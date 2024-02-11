0efa9f7b6c996a6483cc7ac6c6e773b2b906a637
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2024-02-11 12:22:58 +0330

    test functions

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Mohammad-Hakemi22/GenericList/genericlist"
+)
 
 func main() {
 	fmt.Println("Generic list structures")
+	list := genericlist.New[string]()
+
+	list.Insert("alice")
+	list.Insert("bob")
+	list.Insert("alex")
+	fmt.Printf("%+v", list)
+	s, err := list.GetByIndex(1)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s", s)
 }
